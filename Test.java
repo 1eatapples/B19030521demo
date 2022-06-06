e6c54c2b146c322af72fddc2523021b2e20352b7
zhangze <1289671322@qq.com>
2022-06-06 11:13:45 +0800

    完成

--- Test.java
+++ Test.java
@@ -2,7 +2,28 @@
  * @author zz
  */
 public class Test {
-    
+
+    /**
+     * 添加的代码
+     */
+    private String name;
+    private String stuId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStuId() {
+        return stuId;
+    }
+
+    public void setStuId(String stuId) {
+        this.stuId = stuId;
+    }
 
     public static void main(String[] args) {
 
